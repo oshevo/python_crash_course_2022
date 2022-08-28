c9d958e9f439f5134feca2693e2f832596fa2a35
olha babenko <olha.shevo@gmail.com>
2022-08-29 01:41:44 +0200

    comments addded

--- chapter_2/stripping_name.py
+++ chapter_2/stripping_name.py
@@ -1,7 +1,9 @@
-name = ' \tolha babenko\n '
+# program will strip the whitespaces from the string variable
 
-print(name)
+name = ' \tOlha babenko\n ' # announcing variable, adding value with neccesary requirements
 
-print(name.lstrip())
-print(name.rstrip())
-print(name.strip())
+print(name) # printing the result
+
+print(name.lstrip()) # printing with deleting whitespaces on the left part
+print(name.rstrip()) # printing with deleting whitespaces on the right part
+print(name.strip()) # printing with deleting whitespaces from all the word
