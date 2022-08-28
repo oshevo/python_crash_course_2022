14384670535d9c549336f4d02599478b4ff00a6e
olha babenko <olha.shevo@gmail.com>
2022-08-29 00:54:47 +0200

    exercise 2-5 added

---
+++ chapter_2/famous_quote.py
@@ -0,0 +1,12 @@
+# 1st way
+
+quote = 'When we are, death is not come, and, when death is come, we are not.'
+author = 'Epicurus'
+
+print(f'{author} once said, "{quote}"')
+
+# 2nd way
+
+quotation = f'{author} once said, "{quote}"'
+
+print(quotation)
