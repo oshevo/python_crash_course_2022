2ebe050f4ba648989c5a9489df2fa6bfad8ac5d9
olha babenko <olha.shevo@gmail.com>
2022-08-29 00:28:01 +0200

    exercise 2-4 added

---
+++ chapter_2/name_cases.py
@@ -0,0 +1,5 @@
+name = 'olha babenko' # both announcing variable and giving the value to it
+
+print(name.title()) # title method, word begins with upper case
+print(name.upper()) # upper method, whole word is in upper case
+print(name.lower()) # lower method, whole word is in lower case
