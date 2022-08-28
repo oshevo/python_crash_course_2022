3c51dde3a01ffd887082346cac8de08fb15f3d36
olha babenko <olha.shevo@gmail.com>
2022-08-29 00:22:04 +0200

    exercise 2-3 added

---
+++ chapter_2/personal_message.py
@@ -0,0 +1,9 @@
+name = 'olha babenko'
+
+print(f'Hello {name}, how is Python learning going?')
+
+# first_name = 'olha'
+# surname = 'babenko'
+# full_name = f'{first_name} {surname}'
+
+# print(f'Hello {full_name}, how is Python learning going?')
